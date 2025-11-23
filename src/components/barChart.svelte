e95f8c8dc46d456e573041a55781ca5ad4b15c64
verahendriks <hendriksvera@hotmail.com>
2025-11-23 17:21:53 +0100

    styling aanpassen

--- src/components/barChart.svelte
+++ src/components/barChart.svelte
@@ -13,13 +13,13 @@
   let svgContainer; // DOM-container waar de SVG wordt geplaatst
 
   const colors = {
-    averageHours: "#FEA600",
-    maxTemp: "#FF4500",
-    maxUV: "#000000",
+    averageHours: "#FFBC23",
+    maxTemp: "#F45023",
+    maxUV: "#FF9219",
   };
 
   // Marges rondom de grafiek
-  const MARGIN = { top: 25, right: 30, bottom: 50, left: 100 };
+  const MARGIN = { top: 35, right: 15, bottom: 65, left: 145 };
 
   // Hoofd-functie die de grafiek tekent of opnieuw rendert
   function drawChart(chartData) {
@@ -46,7 +46,7 @@
       .scaleBand()
       .domain(chartData.map((d) => d.shortName))
       .range([0, CHART_HEIGHT])
-      .padding(0.2);
+      .padding(0.25);
 
     const maxValue = d3.max(chartData, (d) => d[selectedMetric]);
 
@@ -55,25 +55,40 @@
       .domain([0, maxValue * 1.1])
       .range([0, CHART_WIDTH]);
 
+    // Gridlijnen toevoegen
+    const grid = d3
+      .axisBottom(xScale)
+      .tickSize(-CHART_HEIGHT)
+      .tickFormat("")
+      .ticks(5);
+
+    svg
+      .append("g")
+      .attr("class", "grid-lines")
+      .attr("transform", `translate(0,${CHART_HEIGHT})`)
+      .call(grid)
+      .select(".domain")
+      .remove();
+
+    // Balken tekenen
     const tooltip = d3.select("#d3-tooltip");
 
-    // Bars tekenen + interactie toevoegen
     svg
       .selectAll("rect")
-      .data(chartData, (d) => d.name)
+      .data(chartData)
       .join("rect")
-      .attr("x", 0)
+      .attr("class", "bar")
+      .attr("x", xScale(0))
       .attr("y", (d) => yScale(d.shortName))
       .attr("height", yScale.bandwidth())
-      .attr("width", 0)
+      .attr("rx", 6)
+      .attr("ry", 6)
       .attr("fill", colors[selectedMetric] || "#FEA600")
+      .attr("width", 0)
 
-      // Interactie: hover, move, leave
-      .on("mouseover", function () {
-        d3.select(this).style("opacity", 0.7);
-        tooltip.style("opacity", 1);
-      })
-      .on("mousemove", function (event, d) {
+      // Interactie
+      .on("mouseover", () => tooltip.style("opacity", 1))
+      .on("mousemove", (event, d) => {
         let val = d[selectedMetric];
         let unit = "";
         if (selectedMetric === "averageHours") {
@@ -81,18 +96,19 @@
           unit = " uur";
         }
         if (selectedMetric === "maxTemp") unit = " °C";
-        if (selectedMetric === "totalPrecip") unit = " mm";
         if (selectedMetric === "maxUV") unit = "";
 
-        tooltip.html(`<b>${d.name}</b><br/>${yLabel}: ${val}${unit}`);
+        tooltip
+          .style("left", event.pageX + 15 + "px")
+          .style("top", event.pageY - 15 + "px")
+          .html(`<b>${d.name}</b><br/>${yLabel}: ${val}${unit}`);
       })
-      .on("mouseout", function () {
-        d3.select(this).style("opacity", 1);
-        tooltip.style("opacity", 0);
-      })
-
-      // Klikken: detailpaneel openen
-      .on("click", (_, d) => {
+      .on("mouseout", () => tooltip.style("opacity", 0))
+      .on("click", (event, d) => {
+        if (!d.rawData) {
+          console.log("Geen details voor", d.name);
+          return;
+        }
         selectedCity = d;
         setTimeout(() => {
           document
@@ -100,21 +116,26 @@
             ?.scrollIntoView({ behavior: "smooth" });
         }, 100);
       })
-
-      // Animatie bij het laden
       .transition()
-      .duration(800)
+      .duration(1000)
+      .ease(d3.easeCubicOut)
       .attr("width", (d) => xScale(d[selectedMetric]));
 
-    // Assen tekenen
-    svg
+    // Assen
+    const xAxis = svg
       .append("g")
       .attr("transform", `translate(0,${CHART_HEIGHT})`)
-      .call(d3.axisBottom(xScale))
-      .selectAll("text")
-      .style("text-anchor", "end");
+      .call(d3.axisBottom(xScale).ticks(5));
 
-    svg.append("g").call(d3.axisLeft(yScale));
+    const yAxis = svg
+      .append("g")
+      .attr("class", "axis-y")
+      .call(d3.axisLeft(yScale));
+
+    // Opruimen van assen
+    xAxis.select(".domain").remove();
+    yAxis.select(".domain").remove();
+    yAxis.selectAll(".tick line").remove();
   }
 
   // Automatisch opnieuw tekenen wanneer data verandert
